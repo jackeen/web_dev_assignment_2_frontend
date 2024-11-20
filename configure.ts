03f825f1fae3a90bda8f2a89f2a1e14172a83bbc
Jack <jack.zhang.dev@hotmail.com>
2024-11-21 09:44:22 +1300

    deploy 3

--- configure.ts
+++ configure.ts
@@ -1,2 +1,2 @@
 //export const API_HOST = "http://localhost:8000";
-export const API_HOST = "https://web-development-assignment-2-backend-oxelc7qon.vercel.app";
+export const API_HOST = "https://web-development-assignment-2-backend.vercel.app";
