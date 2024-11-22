c55b86daf04c61e1003de1182d445434ebd7223d
Jack <jack.zhang.dev@hotmail.com>
2024-11-22 15:17:49 +1300

    fix: use 10s timeout for axios

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -3,7 +3,7 @@ import {API_HOST} from "../configure.ts";
 
 const instance = axios.create({
     baseURL: API_HOST,
-    timeout: 5000,
+    timeout: 10000,
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
